d235f1c52bdd44eb4dd37d7c476dc83e10aeeef8
miskogepa <milovan.jankovic50m@gmail.com>
2025-04-21 14:29:39 +0200

    dodavanje ProductList.addEventListener

--- index.js
+++ index.js
@@ -1,28 +1,37 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
+  const products = [
+    { id: 1, name: "Proizvod 1", price: 29.99 },
+    { id: 2, name: "Proizvod 2", price: 19.99 },
+    { id: 3, name: "Proizvod 3", price: 59.99 },
+    { id: 4, name: "Proizvod 4", price: 25.99 },
+    { id: 5, name: "Proizvod 5", price: 30.99 },
+  ];
 
-    const products = [
-        { id: 1, name: 'Proizvod 1', price: 29.99 },
-        { id: 2, name: 'Proizvod 2', price: 19.99 },
-        { id: 3, name: 'Proizvod 3', price: 59.99 },
-        { id: 4, name: 'Proizvod 4', price: 25.99 },
-        { id: 5, name: 'Proizvod 5', price: 30.99 },
-    ];
+  const cart = [];
 
-    const cart = [];
+  const ProductList = document.getElementById("product-list");
+  const CartItems = document.getElementById("cart-items");
+  const emptyCartMessage = document.getElementById("empty-cart");
+  const cartTotalMessage = document.getElementById("cart-total");
+  const totalPriceDisplay = document.getElementById("total-price");
+  const checkoutBtn = document.getElementById("checkout-btn");
 
-    const ProductList = document.getElementById('product-list');
-    const CartItems = document.getElementById('cart-items');
-    const emptyCartMessage = document.getElementById('empty-cart');
-    const cartTotalMessage = document.getElementById('cart-total');
-    const totalPriceDisplay = document.getElementById('total-price');
-    const checkoutBtn = document.getElementById('checkout-btn');
-
-    // Function to render products
-    products.forEach(product => {
-        const productDiv = document.createElement("div")
-        productDiv.classList.add("product")
-        productDiv.innerHTML = `<span>${product.name} -  ${product.price.toFixed(2)} din</span>
+  // Function to render products
+  products.forEach((product) => {
+    const productDiv = document.createElement("div");
+    productDiv.classList.add("product");
+    productDiv.innerHTML = `<span>${product.name} -  ${product.price.toFixed(
+      2
+    )} din</span>
         <button data-id="${product.id}">DODAJ U KORPU</button>`;
-        ProductList.appendChild(productDiv)
-    })
+    ProductList.appendChild(productDiv);
+  });
+
+  ProductList.addEventListener("click", (e) => {
+    if (e.target.tagName === "BUTTON") {
+      const productId = parseInt(e.target.getAttribute("data-id"));
+      const product = products.find((p) => p.id === productId);
+      addToCart(product);
+    }
+  });
 });
